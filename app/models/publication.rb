8aa754bb1754a2a80d3c28541a30ff940eb3f93a
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-14 02:48:00 +0300

    Generate models and update database respectively

---
+++ app/models/publication.rb
@@ -0,0 +1,4 @@
+class Publication < ApplicationRecord
+  belongs_to :category
+  belongs_to :user
+end
--- db/migrate/20210313231000_drop_jwt_denly_list.rb
+++
@@ -1,5 +0,0 @@
-class DropJwtDenlyList < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :jwt_denylists
-  end
-end
