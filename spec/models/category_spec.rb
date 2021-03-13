98cf3bbcea4f73ab89289e668ea6bd0a1883de37
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-14 02:57:48 +0300

    Write model specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it { should have_many(:publications) }
+  end
+
+  context 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(3) }
+    it { should validate_uniqueness_of(:name).case_sensitive }
+  end
 end
--- spec/models/publication_spec.rb
+++ spec/models/publication_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Publication, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it { should belong_to(:category) }
+  end
+
+  context 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:author) }
+    it { should validate_length_of(:title).is_at_least(3) }
+    it { should validate_length_of(:author).is_at_least(3) }
+  end
 end
