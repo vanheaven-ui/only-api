dedd273e526d004c4ce735397a6d378cbeec8c5c
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-14 03:47:49 +0300

    Add comment specs

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  context 'associations' do
+    it { should belong_to(:user) }
+  end
+
+  context 'validations' do
+    it { should validate_presence_of(:body) }
+    it { should validate_length_of(:body).is_at_most(500) }
+  end 
+end
