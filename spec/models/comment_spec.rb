b4186129b3ffaf76c197f4beb2b2de0585823318
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-14 04:00:09 +0300

    Add comments association

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   context 'associations' do
     it { should belong_to(:user) }
+    it { should belong_to(:publication) }
   end
 
   context 'validations' do
