f0f47ea50c84fdbf9df31dddc19632f668e35049
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-14 03:44:04 +0300

    Update specs in respect of Comments aassociation

--- spec/models/publication_spec.rb
+++ spec/models/publication_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Publication, type: :model do
   context 'associations' do
     it { should belong_to(:category) }
+    it { should have_many(:comments).dependent(:destroy) }
   end
 
   context 'validations' do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it { should have_many(:publications).dependent(:destroy) }
+    it { should have_many(:comments).dependent(:destroy) }
+  end
 end
