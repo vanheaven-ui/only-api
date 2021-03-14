714b2dc84a11cc586f5e52b9e1fb12a104a44e41
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-14 12:04:40 +0300

    Implement the Cetegories request spec

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,13 +1,19 @@
+# spec/requests/categories_spec.rb
 require 'rails_helper'
 
-RSpec.describe "Categories API", type: :request do
-  let(:categories) { create_list(:category, 10) }
+RSpec.describe 'Categories API', type: :request do
+  # initialize test data
+  let!(:categories) { create_list(:category, 10) }
   let(:category_id) { categories.first.id }
 
-  describe "GET /api/categories" do
-    before { get "/api/categories" }
+  # Test suite for GET /api/categories
+  describe 'GET /api/categories' do
+    # make HTTP get request before each example
+    before { get '/api/categories' }
 
     it 'returns categories' do
+      # Note `json` is a custom helper to parse JSON responses
+      expect(json).not_to be_empty
       expect(json.size).to eq(10)
     end
 
@@ -16,13 +22,14 @@ RSpec.describe "Categories API", type: :request do
     end
   end
 
+  # Test suite for GET /api/categories/:id
   describe 'GET /api/categories/:id' do
     before { get "/api/categories/#{category_id}" }
 
-    context 'when category exists' do
+    context 'when the record exists' do
       it 'returns the category' do
         expect(json).not_to be_empty
-        expect(json['id']).to eq(category_id) 
+        expect(json['id']).to eq(category_id)
       end
 
       it 'returns status code 200' do
@@ -30,8 +37,8 @@ RSpec.describe "Categories API", type: :request do
       end
     end
 
-    context 'when category does not exist' do
-      let(:catgeory_id) { 50 }
+    context 'when the record does not exist' do
+      let(:category_id) { 100 }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
@@ -43,11 +50,13 @@ RSpec.describe "Categories API", type: :request do
     end
   end
 
-  describe 'POST /api/catgeories' do
-    let(:valid_payload) { { name: 'Sci-Fi' } } 
+  # Test suite for POST /api/categories
+  describe 'POST /api/categories' do
+    # valid payload
+    let(:valid_attributes) { { name: 'Sci-Fi' } }
 
-    context 'when request is valid' do
-      before { post "/api/categories", params: valid_payload }
+    context 'when the request is valid' do
+      before { post '/api/categories', params: valid_attributes }
 
       it 'creates a category' do
         expect(json['name']).to eq('Sci-Fi')
@@ -58,39 +67,33 @@ RSpec.describe "Categories API", type: :request do
       end
     end
 
-    context 'when request is invalid' do
-      let(:invalid_payload) { { name: ''} }
+    context 'when the request is invalid' do
+      before { post '/api/categories', params: {} }
 
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
       end
 
       it 'returns a validation failure message' do
-        expect(response.body).to match(/Validation failed: Name can't be blank/)
+        expect(response.body)
+          .to match(/Validation failed: Name can't be blank, Name is too short (minimum is 3 characters)/)
       end
     end
   end
 
+  # Test suite for PUT /api/categories/:id
   describe 'PUT /api/categories/:id' do
-    let(:valid_payload) { { name: 'History' } }
+    let(:valid_attributes) { { name: 'History' } }
 
-    before { put "/api/categories/#{category_id}", params: valid_payload }
+    context 'when the record exists' do
+      before { put "/api/categories/#{category_id}", params: valid_attributes }
 
-    context 'when category exists' do
-      it 'updates the category' do
+      it 'updates the record' do
         expect(response.body).to be_empty
       end
 
       it 'returns status code 204' do
         expect(response).to have_http_status(204)
-      end
-    end
-
-    context 'when category does not exist' do
-      let(:category_id) { 100 }
-
-      it 'returns a not found message' do
-        expect(response.body).to match(/Couldn't find Category/)
       end
     end
   end
