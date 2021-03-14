ceeadf2650600822c088e33ce0add7a0ed3eeb41
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-14 12:03:35 +0300

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   scope :api, defaults: { format: :json } do
-    resources :categories
+    resources :categories, except: [:destroy]
   end
 end
