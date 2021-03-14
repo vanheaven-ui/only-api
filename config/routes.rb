46c6a5e2dbcd1270eb0cdb8de91d063f0835bb7f
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-14 15:47:52 +0300

    Define REST routes for publications

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   scope :api, defaults: { format: :json } do
+    resources :users do
+      resources :publications
+    end
     resources :categories, except: [:destroy]
   end
 end
